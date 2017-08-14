53a623cbda136501689aee841436ba9182583b72
cbackas <cbackasSE@gmail.com>
2017-08-13 21:47:42 -0500

    Just a command for triggering tests and whatnot

---
+++ src/main/java/cback/commands/CommandTrigger.java
@@ -0,0 +1,40 @@
+package cback.commands;
+
+import cback.TVBot;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IUser;
+
+import java.util.List;
+
+public class CommandTrigger implements Command {
+    @Override
+    public String getName() {
+        return "Trigger";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return null;
+    }
+
+    @Override
+    public String getSyntax() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public List<Long> getPermissions() {
+        return null;
+    }
+
+    @Override
+    public void execute(IMessage message, String content, String[] args, IUser author, IGuild guild, List<Long> roleIDs, boolean isPrivate, IDiscordClient client, TVBot bot) {
+    }
+}
