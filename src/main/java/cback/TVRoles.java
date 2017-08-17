adf8d02fde5fca40f0e4d2e4b93c1183beda6ee1
cbackas <cbackasSE@gmail.com>
2017-08-16 21:09:04 -0500

    Adds TVRoles enum

---
+++ src/main/java/cback/TVRoles.java
@@ -0,0 +1,28 @@
+package cback;
+
+public enum TVRoles {
+    STAFF("STAFF", 227213155917496330l),
+    ADMIN("ADMIN", 192441946210435072l),
+    HEADMOD("HEADMOD", 263126026261889024l),
+    NETWORKMOD("NETWORKMOD", 264130466062401536l),
+    MOD("MOD", 192442068981776384l),
+    MOVIENIGHT("MOVIENIGHT", 226443478664609792l),
+    REDDITMOD("REDDITMOD", 221973215948308480l);
+
+    public String name;
+    public Long id;
+
+    TVRoles(String name, Long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public static TVRoles getRole(String name) {
+        for (TVRoles role : values()) {
+            if (role.name.equalsIgnoreCase(name)) {
+                return role;
+            }
+        }
+        return null;
+    }
+}
