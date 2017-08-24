658d6e2a2e74362fea40e32ffdddbbd78d6df2b4
cbackas <cbackasSE@gmail.com>
2017-08-24 17:26:58 -0500

    Todo lists added

--- src/main/java/cback/events/ChannelChange.java
+++ src/main/java/cback/events/ChannelChange.java
@@ -19,32 +19,13 @@ public class ChannelChange {
         this.bot = bot;
     }
 
-    @EventSubscriber //Set all
-    public void setMuteRoleMASS(MessageReceivedEvent event) {
-            IMessage message = event.getMessage();
-            String text = message.getContent();
-            IDiscordClient client = event.getClient();
-            if (text.equalsIgnoreCase("?setmuteperm") && message.getAuthor().getStringID().equals("73416411443113984")) {
-                IGuild guild = client.getGuildByID(Long.parseLong("266649217538195457"));
-                List<IChannel> channelList = guild.getChannels();
-                IRole muted = guild.getRoleByID(Long.parseLong("281022564002824192"));
-                for (IChannel channels : channelList) {
-                    RequestBuffer.request(() -> {
-                        try {
-                            channels.overrideRolePermissions(muted, EnumSet.noneOf(Permissions.class), EnumSet.of(Permissions.SEND_MESSAGES));
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    });
-                }
-                System.out.println("Set muted role");
-                Util.deleteMessage(message);
-            }
-    }
-
-    @EventSubscriber //New Channel
+    @EventSubscriber
     public void newChannel(ChannelCreateEvent event) {
-        //Set muted role
+        /**
+         * Assign muted role settings to new channels
+         * todo make muted role id friendlier to other servers
+         * todo make guild role a public thing for each bot
+         */
         IGuild guild = event.getClient().getGuildByID(Long.parseLong("266649217538195457"));
         IRole muted = guild.getRoleByID(Long.parseLong("281022564002824192"));
         try {
@@ -52,7 +33,33 @@ public class ChannelChange {
         } catch (Exception e) {
             Util.reportHome(e);
         }
+    }
 
+    /**
+     * Hardcoded command to force the muted role's settings upon every channel the bot can get it's hands on
+     * todo decide what to do with this
+     */
+    @EventSubscriber
+    public void setMuteRoleMASS(MessageReceivedEvent event) {
+        IMessage message = event.getMessage();
+        String text = message.getContent();
+        IDiscordClient client = event.getClient();
+        if (text.equalsIgnoreCase("?setmuteperm") && message.getAuthor().getStringID().equals("73416411443113984")) {
+            IGuild guild = client.getGuildByID(Long.parseLong("266649217538195457"));
+            List<IChannel> channelList = guild.getChannels();
+            IRole muted = guild.getRoleByID(Long.parseLong("281022564002824192"));
+            for (IChannel channels : channelList) {
+                RequestBuffer.request(() -> {
+                    try {
+                        channels.overrideRolePermissions(muted, EnumSet.noneOf(Permissions.class), EnumSet.of(Permissions.SEND_MESSAGES));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+            System.out.println("Set muted role");
+            Util.deleteMessage(message);
+        }
     }
 
 }
